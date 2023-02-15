5680e0ea13fdc14deb4d9bac2aa5c27d0299b61e
Gustavo Nobrega <gustavobiel1@outlook.com>
2023-02-15 17:11:44 -0300

    Concluindo a page Feedbaks

--- src/router/index.js
+++ src/router/index.js
@@ -6,43 +6,49 @@ import BookView from "../views/Book/BookView.vue";
 import ContactView from "../views/Contact/ContactView.vue";
 import SignView from "../views/Sign/SignView.vue";
 import RegisterView from "../views/Register/RegisterView.vue";
+import DeluxeView from "../views/Feedback/DeluxeView.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: HomeView,
-    },
-    {
-        path: "/sobre",
-        name: "sobre",
-        component: AboutView,
-    },
-    {
-        path: "/acomodacoes",
-        name: "acomodacoes",
-        component: RoomsView,
-    },
-    {
-        path: "/reservar",
-        name: "reservar",
-        component: BookView,
-    },
-    {
-        path: "/contato",
-        name: "contato",
-        component: ContactView,
-    },
-    {
-        path: "/acessar",
-        name: "acessar",
-        component: SignView,
-    },
-    {
-        path: "/cadastrar",
-        name: "cadastro",
-        component: RegisterView,
-    },
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/sobre",
+    name: "sobre",
+    component: AboutView,
+  },
+  {
+    path: "/acomodacoes",
+    name: "acomodacoes",
+    component: RoomsView,
+  },
+  {
+    path: "/reservar",
+    name: "reservar",
+    component: BookView,
+  },
+  {
+    path: "/contato",
+    name: "contato",
+    component: ContactView,
+  },
+  {
+    path: "/acessar",
+    name: "acessar",
+    component: SignView,
+  },
+  {
+    path: "/cadastrar",
+    name: "cadastro",
+    component: RegisterView,
+  },
+  {
+    path: "/feedback",
+    name: "feedback",
+    component: DeluxeView,
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/Feedback/DeluxeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <MainHeader />
+  <main>
+    <section class="feedback-hero cover-background">
+      <h1 class="feedback-title">Confortável como um Abraço!</h1>
+    </section>
+
+    <div class="container-feedback">
+      <div class="content-feedback">
+        <div>
+          <h2>Standard</h2>
+        </div>
+      </div>
+    </div>
+    <div class="content-form">
+        <FormFeedback />
+    </div>
+  </main>
+  <MainFooter />
+</template>
+
+<script>
+import MainHeader from "@/components/MainHeader.vue";
+import FormFeedback from "@/components/FormFeedback.vue";
+import MainFooter from "@/components/MainFooter.vue";
+
+export default {
+  name: "DeluxeView",
+  components: {
+    MainHeader,
+    FormFeedback,
+    MainFooter,
+  },
+};
+</script>
+
+<style src="./style.scss" lang="scss" />
